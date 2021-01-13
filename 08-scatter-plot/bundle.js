9e1bbdbdf08439d7c1fd4f1beb85a2124dcf2e84
Takuya Kitazawa <k.takuti@gmail.com>
2021-01-14 07:19:39 +0900

    Scatter plot

---
+++ 08-scatter-plot/bundle.js
@@ -0,0 +1,144 @@
+(function (React$1, ReactDOM, d3) {
+  'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var React__default = /*#__PURE__*/_interopDefaultLegacy(React$1);
+  var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
+
+  var csvUrl =
+    'https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv';
+
+  var useData = function () {
+    var ref = React$1.useState(null);
+    var data = ref[0];
+    var setData = ref[1];
+
+    React$1.useEffect(function () {
+      var row = function (d) {
+        d.sepal_length = +d.sepal_lengh;
+        d.sepal_width = +d.sepal_width;
+        d.petal_length = +d.petal_length;
+        d.petal_width = +d.petal_width;
+        return d;
+      };
+      d3.csv(csvUrl, row).then(setData);
+    }, []);
+
+    return data;
+  };
+
+  var AxisBottom = function (ref) {
+      var xScale = ref.xScale;
+      var innerHeight = ref.innerHeight;
+      var tickOffset = ref.tickOffset; if ( tickOffset === void 0 ) tickOffset = 3;
+
+      return xScale.ticks().map(function (tickValue) { return (
+      React.createElement( 'g', {
+        className: "tick", key: tickValue, transform: ("translate(" + (xScale(tickValue)) + ",0)") },
+        React.createElement( 'line', { y2: innerHeight }),
+        React.createElement( 'text', {
+          style: { textAnchor: 'middle' }, dy: ".71em", y: innerHeight + tickOffset },
+          tickValue
+        )
+      )
+    ); });
+  };
+
+  var AxisLeft = function (ref) {
+      var yScale = ref.yScale;
+      var innerWidth = ref.innerWidth;
+      var tickOffset = ref.tickOffset; if ( tickOffset === void 0 ) tickOffset = 3;
+
+      return yScale.ticks().map(function (tickValue) { return (
+      React.createElement( 'g', {
+        className: "tick", transform: ("translate(0," + (yScale(tickValue)) + ")") },
+        React.createElement( 'line', { x2: innerWidth }),
+        React.createElement( 'text', {
+          key: tickValue, style: { textAnchor: 'end' }, x: -tickOffset, y: yScale(tickValue), dy: ".32em" },
+          tickValue
+        )
+      )
+    ); });
+  };
+
+  var Marks = function (ref) {
+      var data = ref.data;
+      var xScale = ref.xScale;
+      var yScale = ref.yScale;
+      var xValue = ref.xValue;
+      var yValue = ref.yValue;
+      var circleRadius = ref.circleRadius;
+
+      return data.map(function (d) { return (
+      React.createElement( 'circle', {
+        className: "mark", cx: xScale(xValue(d)), cy: yScale(yValue(d)), r: circleRadius })
+    ); });
+  };
+
+  var width = 960;
+  var height = 500;
+  var margin = {
+    top: 20,
+    right: 30,
+    bottom: 80,
+    left: 100,
+  };
+  var xAxisOffset = 60;
+  var yAxisOffset = 50;
+
+  var tickOffset = 16;
+
+  var App = function () {
+    var data = useData();
+
+    if (!data) {
+      return React__default['default'].createElement( 'pre', null, "Loading...." );
+    }
+
+    var innerHeight = height - margin.top - margin.bottom;
+    var innerWidth = width - margin.right - margin.left;
+
+    var xValue = function (d) { return d.petal_length; };
+    var xAxisLabel = 'Petal Length';
+
+    var yValue = function (d) { return d.sepal_width; };
+    var yAxisLabel = 'Sepal Length';
+
+    var xScale = d3.scaleLinear()
+      .domain(d3.extent(data, xValue))
+      .range([0, innerWidth])
+      .nice();
+
+    var yScale = d3.scaleLinear()
+      .domain(d3.extent(data, yValue))
+      .range([0, innerHeight]);
+
+    return (
+      React__default['default'].createElement( 'svg', { width: width, height: height },
+        React__default['default'].createElement( 'g', {
+          transform: ("translate(" + (margin.left) + "," + (margin.top) + ")") },
+          React__default['default'].createElement( AxisBottom, {
+            xScale: xScale, innerHeight: innerHeight, tickOffset: tickOffset }),
+          React__default['default'].createElement( 'text', {
+            className: "axis-label", x: innerWidth / 2, y: innerHeight + xAxisOffset, textAnchor: "middle" },
+            xAxisLabel
+          ),
+          React__default['default'].createElement( AxisLeft, {
+            yScale: yScale, innerWidth: innerWidth, tickOffset: tickOffset }),
+          React__default['default'].createElement( 'text', {
+            className: "axis-label", textAnchor: "middle", transform: ("translate(" + (-yAxisOffset) + "," + (innerHeight / 2) + ") rotate(-90)") },
+            yAxisLabel
+          ),
+          React__default['default'].createElement( Marks, {
+            data: data, xScale: xScale, yScale: yScale, xValue: xValue, yValue: yValue, circleRadius: 7 })
+        )
+      )
+    );
+  };
+
+  var rootElement = document.getElementById('root');
+  ReactDOM__default['default'].render(React__default['default'].createElement( App, null ), rootElement);
+
+}(React, ReactDOM, d3));
+//# sourceMappingURL=bundle.js.map
---
+++ 08-scatter-plot/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { scaleBand, scaleLinear, extent } from 'd3';
+import { useData } from './useData';
+import { AxisBottom } from './AxisBottom';
+import { AxisLeft } from './AxisLeft';
+import { Marks } from './Marks';
+
+const width = 960;
+const height = 500;
+const margin = {
+  top: 20,
+  right: 30,
+  bottom: 80,
+  left: 100,
+};
+const xAxisOffset = 60;
+const yAxisOffset = 50;
+
+const tickOffset = 16;
+
+const App = () => {
+  const data = useData();
+
+  if (!data) {
+    return <pre>Loading....</pre>;
+  }
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.right - margin.left;
+
+  const xValue = (d) => d.petal_length;
+  const xAxisLabel = 'Petal Length';
+
+  const yValue = (d) => d.sepal_width;
+  const yAxisLabel = 'Sepal Length';
+
+  const xScale = scaleLinear()
+    .domain(extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = scaleLinear()
+    .domain(extent(data, yValue))
+    .range([0, innerHeight]);
+
+  return (
+    <svg width={width} height={height}>
+      <g
+        transform={`translate(${margin.left},${margin.top})`}
+      >
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickOffset={tickOffset}
+        />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisOffset}
+          textAnchor="middle"
+        >
+          {xAxisLabel}
+        </text>
+        <AxisLeft
+          yScale={yScale}
+          innerWidth={innerWidth}
+          tickOffset={tickOffset}
+        />
+        <text
+          className="axis-label"
+          textAnchor="middle"
+          transform={`translate(${-yAxisOffset},${
+            innerHeight / 2
+          }) rotate(-90)`}
+        >
+          {yAxisLabel}
+        </text>
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+          circleRadius={7}
+        />
+      </g>
+    </svg>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
