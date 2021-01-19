6c5498984e5b2823ff00fef2446b6aef7a58d50f
Takuya Kitazawa <k.takuti@gmail.com>
2021-01-20 05:56:43 +0900

    Refactor

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-const buble = require('@rollup/plugin-buble');
+import buble from '@rollup/plugin-buble';
 
 const buildFolders = [
   '01-face',
@@ -15,7 +15,7 @@ const buildFolders = [
   '12-choropleth-map'
 ];
 
-const configs = buildFolders.map(v => {
+export default buildFolders.map(v => {
   return {
     input: v + '/index.js',
     external: ["d3","d3-array","d3-axis","d3-brush","d3-chord","d3-collection","d3-color","d3-contour","d3-dispatch","d3-drag","d3-dsv","d3-ease","d3-fetch","d3-force","d3-format","d3-geo","d3-hierarchy","d3-interpolate","d3-path","d3-polygon","d3-quadtree","d3-random","d3-scale","d3-scale-chromatic","d3-selection","d3-shape","d3-tile","d3-time","d3-time-format","d3-timer","d3-transition","d3-voronoi","d3-zoom","react","react-dom","react-dropdown-browser","react-dropdown","three","vega","vega-embed","vega-lite","vega-lite-api","vega-tooltip","vega-themes","vizhub-vega-lite-config","semiotic","viz.js","topojson"],
@@ -34,5 +34,3 @@ const configs = buildFolders.map(v => {
     ]
   }
 });
-
-export default configs;
